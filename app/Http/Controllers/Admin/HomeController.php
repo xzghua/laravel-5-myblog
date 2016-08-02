f611f97768ec40b17167edafd84348da5f24b22a
Yela <2067930913@qq.com>
2016-08-03 02:28:39 +0800

    no password test

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -17,6 +17,7 @@ class HomeController extends Controller
     public function index()
     {
         //
+        \Symfony\Component\Debug\Tests\FatalErrorHandler\test_namespaced_function();
     }
 
     /**
